5273771a82cf8d4596a91819e8b5bc877fcd40ef
DoanDong123 <doanquydong@gmail.com>
2020-07-27 11:20:58 +0700

    update code 2707

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -15,6 +15,60 @@ def read_products(keyword=None, from_price=None, to_price=None):
 
     return products
 
+
+
+
+def read_categories():
+    with open(os.path.join(app.root_path,'data/categories.json'), encoding="utf-8") as f:
+        categories = json.load(f)
+    return categories
+
+def read_product_by_id(product_id):
+    products = read_products()
+    for p in products:
+        if p["id"] == product_id:
+            return p
+    return None
+
+def add_products (name, description, price, image, category):
+    products = read_products()
+
+    products.append({
+        "id": len(products) + 1,
+        "name": name,
+        "description": description,
+        "price": float(price),
+        "image": image,
+        "category_id": int(category)
+    })
+
+    return update_product_json(products)
+
+def update_product(product_id, name, description, price, image, category):
+    products =read_products()
+    for p in products:
+        if p["id"] == int(product_id):
+            p["name"] = name
+            p["description"] = description
+            p["price"] = float(price)
+            p["image"] = image
+            p["category"] = int(category)
+
+            break
+
+    return update_product_json(products)
+
+def update_product_json(products):
+    try:
+        with open(os.path.join(app.root_path, 'data/products.json'), "w", encoding="utf-8") as f:
+            json.dump(products, f, ensure_ascii=False, indent=4)
+
+        return True
+    except:
+        ex = Exception
+        return False
+
+
 def read_products_by_cate_id(cate_id):
     return [product for product in read_products() if product["category_id"] == cate_id]
     # products = read_products()
--- saleapp/main.py
+++ saleapp/main.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from saleapp import app
 from saleapp import dao
 
@@ -6,6 +6,7 @@ from saleapp import dao
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/products")
 def product_list():
@@ -19,9 +20,39 @@ def product_list():
                                                                            from_price=from_price,
                                                                            to_price=to_price))
 
+
+@app.route("/products/add", methods=["get", "post"])
+def product_add():
+    err_msg = None
+    if request.method.lower() == "post":
+        if request.args.get("product_id"): # Update
+            d = dict(request.form.copy())
+            d["product_id"] = int(request.args["product_id"])
+            if dao.update_product(**d):
+                return redirect(url_for('product_list'))
+            else:
+                err_msg = "Somthing wrong"
+        else: # ADD
+            if dao.add_products(**dict(request.form)):
+                return redirect(url_for('product_list'))
+            else:
+                err_msg = "Somthing wrong"
+
+    categories = dao.read_categories()
+
+    product = None
+    if request.args["product_id"]:
+
+        product = dao.read_product_by_id(int(request.args["product_id"]))
+
+    return render_template("product-add.html", categories=categories, product=product, err_msg=err_msg)
+
+
+
 @app.route("/products/<int:category_id>")
 def product_list_by_cate(category_id):
     return render_template("product-list.html", products=dao.read_products_by_cate_id(cate_id=category_id))
+
 
 
 if __name__ == "__main__":
